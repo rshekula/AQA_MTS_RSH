6bb08f401b42b7cfbb859fa0358c00b92d92a133
RShekula <151993086+rshekula@users.noreply.github.com>
2023-12-27 12:43:20 +0300

    Update one.cs
    добавил параметр игнорирования регистра

--- Strings/one.cs
+++ Strings/one.cs
@@ -1,4 +1,4 @@
-﻿using System.Text;
+using System.Text;
 using System.Text.RegularExpressions;
 using static System.Net.Mime.MediaTypeNames;
 
@@ -13,9 +13,7 @@ namespace Strings
             string one = "1 test, 2 Test, 3 TesT, 4 TiST, 5 no_test, finish";
             Console.WriteLine($"Изначальная строка: {one}");
 
-            string none = one.ToLower(); // подогнал всё под нижний регистр, чтобы поменялись все слова test (кроме 4)
-
-            string two = none.Replace("test", "testing");
+            string two = one.Replace("test", "testing", StringComparison.OrdinalIgnoreCase);
             two = Regex.Replace(two, @"\d+", "");
 
             Console.WriteLine($"Новая строка: {two}");
