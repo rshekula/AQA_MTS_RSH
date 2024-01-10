60a37379c73878101b47e94bd6da801a3168635d
Rshekula <rshekula@gmail.com>
2024-01-10 18:14:15 +0300

    Finish

---
+++ Exceptions/Exceptions/Authorization.cs
@@ -0,0 +1,37 @@
+﻿using static Exceptions.WrongLoginException;
+using static Exceptions.WrongPasswordException;
+
+namespace Exceptions
+{
+    public static class Authorization
+    {
+        public static bool CheckInputInfo(string login, string password, string confirmPassword)
+        {
+            bool result = true;
+
+            if (login.Length >= 20 || login.Contains(' '))
+            {
+                result = false;
+                throw new WrongLoginException("Логин должен быть меньше 20 символов и не содержать пробелы");
+            }
+            if (password.Length >= 20 || password.Contains(' '))
+            {
+                result = false;
+                throw new WrongPasswordException("Пароль должен быть меньше 20 символов и не содержать пробелы");
+            }
+            if (password.All(x => !char.IsDigit(x)))
+            {
+                result = false;
+                throw new WrongPasswordException("Пароль должен содержать хотя бы одну цифру");
+            }
+            if (password != confirmPassword)
+            {
+                result = false;
+                throw new WrongPasswordException("Пароль не совпадает");
+            }
+
+            return result;
+        }
+            
+    }
+}
