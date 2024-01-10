0bd9842f194d41c37bf7b54602abd1a0acf7a328
Rshekula <rshekula@gmail.com>
2024-01-10 15:39:45 +0300

    Finish

---
+++ Exceptions/Exceptions/CheckedExceptions.cs
@@ -0,0 +1,24 @@
+﻿namespace Exceptions;
+
+public class CheckedExceptions
+{
+    public void ReadFile(string filePath)
+    {
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            Console.WriteLine(reader.ReadToEnd());
+        }
+    }
+
+    public void correctReadFile(string filePath)
+    {
+        try
+        {
+            ReadFile(filePath);
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine($"Ошибка ввода-вывода: {ex.Message}");
+        }
+    }
+}
---
+++ Exceptions/Exceptions/CustomArgumentException.cs
@@ -0,0 +1,12 @@
+﻿namespace Exceptions;
+
+public class CustomArgumentException : ArgumentException
+{
+    public int Value { get; }
+
+    public CustomArgumentException(string message, int val)
+        : base(message)
+    {
+        Value = val;
+    }
+}
